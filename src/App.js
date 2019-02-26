e7ff1823a66e3f24f2c2be3a52694e274d65c0f8
kaleido <mbdkaleido@gmail.com>
2019-02-26 11:20:19 +0900

    koko ni hozon sitai komento wo kakuyo

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello world! </h1>
+          <h1>Hello world!今日は元気ですか？ </h1>
           <a
             className="App-link"
             href="https://reactjs.org"
