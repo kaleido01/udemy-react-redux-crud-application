e15569531932e4ba0725741e4e37c6bbfd635031
kaleido <kaleido@gmail.com>
2019-03-26 13:56:01 +0900

    learn  react jsx

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 
+//JSX version
+
 class App extends Component {
-  render() {
+  render() 
+  {
+    const greeting ="Hi TOM!";
+    const dom=<h1 className="foo">{greeting}</h1>;
+
+
     return (
-      <div>
-          <h1>Hello world</h1>
-      </div>
-    );
+    <React.Fragment>
+        <input type="text" onChange={()=>{console.log("I am clicked")}}/>
+     
+
+    </React.Fragment>
+    
+    
+     
+    )
   }
 }
 
-export default App;
+ export default App;
